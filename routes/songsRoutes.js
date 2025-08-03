adf01e2b40bd2537de545222367f2a59c349e881
Krishna Kr Das <132001027+KrishnaDas159@users.noreply.github.com>
2025-08-03 16:25:21 +0530

    Update songsRoutes.js

--- routes/songsRoutes.js
+++ routes/songsRoutes.js
@@ -1,13 +1,10 @@
-// routes/songsRoutes.js
+
 import express from "express";
-import multer from "multer";
-import { addSong, getSongs} from "../controllers/songsController.js";
+
+import {getSongs} from "../controllers/songsController.js";
 
 const router = express.Router();
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
 
-router.post("/", upload.fields([{ name: "cover", maxCount: 1 }, { name: "url", maxCount: 1 }]), addSong);
 router.get("/", getSongs);
 
 
